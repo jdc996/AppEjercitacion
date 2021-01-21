899469c1d16ee046cdc7da0ea3d8bf65c0ff4460
Juan David Cubiilos Calvachi <jdcubillos96@gmail.com>
2021-01-21 17:12:22 -0500

    Practica 2 algoritmo de listas

--- src/controller/practica2.java
+++ src/controller/practica2.java
@@ -25,16 +25,40 @@ public class practica2 {
     }
 
     public static List<String> crearListas(List<String> numeros) {
-        Map listas = new HashMap<Integer,List<String>>();
+        Map<String,List<String>> listas = new HashMap();
+        List<String> listaOrdenada = new ArrayList<>();
+        int contIter = numeros.get(0).length() - 1;
+        System.out.println(contIter);
+        for (String numero:numeros) {
+            String digito = String.valueOf(numero.charAt(contIter));
+            if (listas.containsKey(digito)){
+                ArrayList<String> lista = (ArrayList<String>) listas.get(digito);
+                lista.add(numero);
+            }else {
+                List<String> li=new ArrayList<String>();
+                li.add(numero);
+                listas.put(digito,li);
+            }
+        }
+
+        return null;
+    }
+
+    public static List<String> crearListaOrdenada(Map<String,List<String>> listas){
+        for (Map.Entry<String,List<String>> entry: listas.entrySet()){
+            System.out.println("Key "+entry.getKey() + " Valor" +entry.getValue());
+
+        }
         return null;
     }
 
     public static void radixSort(int []arr){
         List<String> numerosString = convertirArrayString(arr);
         System.out.println(numerosString);
+        crearListas(numerosString);
     }
     public static void main(String[] args) {
-        int[] numerosArreglo = {1,2,32423,423,54,2};
+        int[] numerosArreglo = {2,32423,423,54,2,1};
         radixSort(numerosArreglo);
 
     }
